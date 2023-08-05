b58dfd00e5ed7091c2a43d359bae3eedf3da7e93
severo <oliveira.severo@gmail.com>
2023-08-05 14:46:17 -0300

    feat: :rocket: Cria os arquivos base do Tema

---
+++ wp-content/themes/keevoblog/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no" />
+		<title>keevo-Blog</title>
+	</head>
+	<body>
+    <h1>Inicio do projeto</h1>
+	</body>
+</html>
