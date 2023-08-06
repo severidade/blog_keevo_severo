f6d5c0284da4ec1e2ec63712f75f156c305fecf0
severo <oliveira.severo@gmail.com>
2023-08-05 21:28:15 -0300

    feat: :rocket: Cria o loop para exibir os posts

--- wp-content/themes/keevoblog/index.php
+++ wp-content/themes/keevoblog/index.php
@@ -6,7 +6,22 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur eum itaque quidem ad maiores facilis ipsum vitae dicta ex, voluptate, quaerat officiis molestias obcaecati veniam hic ullam nostrum modi tempore</p>
   </section>
   <section class="blog_posts">
-    lista dos posts do blog
+    <?php 
+      if(have_posts()) : 
+        while (have_posts()) : the_post();
+    ?>
+      <div class="post">
+        <h1><?php the_title(); ?></h1>
+        <p>Publicado em <?php echo get_the_date(); ?> por <?php the_author() ?></p>
+        <p>Categorias: <?php the_category(' '); ?> </p>
+        <p><?php the_tags('Tags: ', ', '); ?></p>
+        <p><?php the_content(); ?></p>
+      </div>
+    <?php endwhile;
+      else:
+    ?>
+      <p>Não Há posts</p>
+    <?php endif; ?>
   </section>
 </main>
 <?php get_footer() ?>
