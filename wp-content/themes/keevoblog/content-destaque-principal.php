52c7709b5edc9feb68d19de2e8c86b4e95b0e516
severo <oliveira.severo@gmail.com>
2023-08-07 20:21:02 -0300

    feat: :rocket: Cria os links pra a página single.php

--- wp-content/themes/keevoblog/content-destaque-principal.php
+++ wp-content/themes/keevoblog/content-destaque-principal.php
@@ -1,10 +1,12 @@
 <article class="noticia_destaque_principal">
   <div class="img_container_destaque_principal">
-    <?php the_post_thumbnail('large'); ?>
+    <a href="<?php the_permalink(); ?>">
+      <?php the_post_thumbnail('large'); ?>
+    </a>
   </div>
   <div class="text_container_destaque_principal">
-    <h1 class="post_title"><?php the_title(); ?></h1>
-    <p>por <samp><?php the_author() ?></samp> em <span><?php the_category(' '); ?></span> Tags: <span><?php the_tags('Tags: ', ', '); ?></span> </p>
+    <h1 class="post_title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+    <p>por <samp><?php the_author_posts_link() ?></samp> em <span><?php the_category(' '); ?></span> Tags: <span><?php the_tags('Tags: ', ', '); ?></span> </p>
     <p>Publicado em <?php echo get_the_date(); ?></p>
     <p><?php the_excerpt(); ?></p>
   </div>
--- wp-content/themes/keevoblog/post_list.php
+++ wp-content/themes/keevoblog/post_list.php
@@ -1,11 +1,13 @@
 <article>
   <div class="post">
-    <h1 class="post_title"><?php the_title(); ?></h1>
+    <h1 class="post_title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <div class="post_img_container">
+    <a href="<?php the_permalink(); ?>">
       <?php the_post_thumbnail('thumbnail'); ?>
+    </a>
     </div>
     <div class="post_text_container">
-      <p>Publicado em <?php echo get_the_date(); ?> por <?php the_author() ?></p>
+      <p>Publicado em <?php echo get_the_date(); ?> por <?php the_author_posts_link() ?></p>
       <p>Categorias: <?php the_category(' '); ?> </p>
       <p><?php the_tags('Tags: ', ', '); ?></p>
       <p><?php the_excerpt(); ?></p>
