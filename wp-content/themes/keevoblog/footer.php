c7f070a3b087262b2e27e1257a5ab3a8fe8b5d0a
severo <oliveira.severo@gmail.com>
2023-08-08 00:08:38 -0300

    feat: :rocket: Atualiza links do rodapé

--- wp-content/themes/keevoblog/footer.php
+++ wp-content/themes/keevoblog/footer.php
@@ -1,10 +1,10 @@
       <footer>
         <div class="social_links">
           <ul>
-            <li><a href="#" target="_blank">Facebook</a></li>
-            <li><a href="#" target="_blank">Instagram</a></li>
-            <li><a href="#" target="_blank">Linkedin</a></li>
-            <li><a href="#" target="_blank">Youtube</a></li>
+            <li><a href="https://www.facebook.com/keevosoftware" target="_blank">Facebook</a></li>
+            <li><a href="https://www.instagram.com/keevosoftware/" target="_blank">Instagram</a></li>
+            <li><a href="https://www.linkedin.com/company/keevosoftware/" target="_blank">Linkedin</a></li>
+            <li><a href="https://www.youtube.com/keevo" target="_blank">Youtube</a></li>
           </ul>
         </div>
         <div class="autor"><p>Desenvolvido por <a href="https://www.linkedin.com/in/severidade/" target="_blank">severo</a></p></div>
