93f3de80cf37e664973c9035c7cb80a5e63298d7
severo <oliveira.severo@gmail.com>
2023-08-08 00:30:36 -0300

    feat: :rocket: insere imagem de destaque no post

--- wp-content/themes/keevoblog/content-single.php
+++ wp-content/themes/keevoblog/content-single.php
@@ -1,4 +1,7 @@
 <article class="post_single">
+  <div class="img_single_destaque">
+    <?php the_post_thumbnail('large'); ?>
+  </div>
   <header>
     <h1 class="post_title"><?php the_title(); ?></h1>
     <p>Publicado por <?php the_author_posts_link() ?> em <?php echo get_the_date(); ?></p>
