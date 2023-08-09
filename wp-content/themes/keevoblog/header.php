f6732150de10fbe05e4cbaf89b5011dd4721e520
severo <oliveira.severo@gmail.com>
2023-08-09 08:49:12 -0300

    feat: :rocket: Implementa menu interaçao js

--- wp-content/themes/keevoblog/header.php
+++ wp-content/themes/keevoblog/header.php
@@ -10,11 +10,35 @@
 <div class="container_blog">
   <header>
     <div class="container_logo">
-    <a href="<?php echo esc_url(home_url('/')); ?>" class="logo">
-      <h1>Logo do Blog</h1>
-    </a>
+      <a href="<?php echo esc_url(home_url('/')); ?>" class="logo">
+        <h1>Keevo | Blog</h1>
+      </a>
+      <div id="menu-control-open">hamburger</div>  
     </div>
-    <nav class="menu_area">
+    <nav id="menu_area">
+      <div id="menu-control-close">Fechar</div>
       <?php wp_nav_menu( array( 'theme_location' => 'meu_menu_principal' )); ?>
     </nav>
   </header>
+
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      // Alvo
+      const menuControlOpen = document.getElementById("menu-control-open");
+      const menuControlClose = document.getElementById("menu-control-close");
+
+      // Escutadores
+      menuControlOpen.addEventListener("click", togleMenu)
+      menuControlClose.addEventListener("click", togleMenu)
+
+
+      function togleMenu(event) {
+        document.getElementById("menu_area").classList.toggle("active");
+        document.getElementsByTagName("body")[0].classList.toggle("boqueiaScroll");
+      }
+
+      // closeModal.addEventListener('click', function() {
+      //   successModal.style.display = 'none';
+      // });
+    });
+  </script>
